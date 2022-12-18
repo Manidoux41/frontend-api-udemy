ac16b9516eb988d24902ce38e5c32c6a4db2a991
superflyman90 <superflyman90@gmail.com>
2022-12-18 07:45:58 +0100

    add antdesign

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
+import App from './App';
+import { BrowserRouter as Router } from 'react-router-dom';
+import 'antd/dist/reset.css';
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <App />
+    </Router>
   </React.StrictMode>,
 )
