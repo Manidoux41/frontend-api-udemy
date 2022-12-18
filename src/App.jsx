66c27a9e85d295bc6d054ca52f52f72ef5dbc95b
superflyman90 <superflyman90@gmail.com>
2022-12-18 08:52:46 +0100

    implement react router

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,21 @@
 import React from 'react'
 import MPLayout from './components/Layout';
+import { Routes, Route } from 'react-router-dom'
+import HomePage from './pages/HomePage';
+import RoomsPage from './pages/RoomsPage'
 
 const App = () => {
   return (
 
     <MPLayout>
-      <h1>Home</h1>
+      <Routes>
+        <Route path='/' element={<HomePage />} />
+        <Route path='/rooms' element={<RoomsPage /> } />
+        <Route path='/about' element={<h1>A Propos</h1>} />
+        <Route path='/contact' element={<h1>Contact</h1>} />
+        <Route path='*' element={<h1>Not Found 404</h1>} />
+
+      </Routes>
     </MPLayout>
   )
 }
---
+++ src/pages/RoomsPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import {Routes, Route} from 'react-router-dom';
+
+const RoomsPage = () => {
+  return (
+    <Routes>
+        <Route path='/' element={<h2>Rooms</h2>} />
+    </Routes>
+  )
+}
+
+export default RoomsPage
