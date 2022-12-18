0d6ee113b1978fbc4daa64cbdcc55cc31e699c1d
superflyman90 <superflyman90@gmail.com>
2022-12-18 09:04:37 +0100

    implement react id rooms3

--- src/App.jsx
+++ src/App.jsx
@@ -10,9 +10,9 @@ const App = () => {
     <MPLayout>
       <Routes>
         <Route path='/' element={<HomePage />} />
-        <Route path='/rooms/*' element={<RoomsPage /> } />
-        <Route path='/about' element={<h1>A Propos</h1>} />
-        <Route path='/contact' element={<h1>Contact</h1>} />
+        <Route path='rooms/*' element={<RoomsPage /> } />
+        <Route path='about' element={<h1>A Propos</h1>} />
+        <Route path='contact' element={<h1>Contact</h1>} />
         <Route path='*' element={<h1>Not Found 404</h1>} />
 
       </Routes>
