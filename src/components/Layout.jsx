aca88e97708cfec6ae592d92bca08e9d531b8fc2
superflyman90 <superflyman90@gmail.com>
2022-12-18 08:33:10 +0100

    layout ok and menu ok

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,11 +1,41 @@
 import React from 'react'
-import { Layout } from 'antd';
+import { Link } from 'react-router-dom'
+import { Layout, Menu } from 'antd';
 
+const { Header, Footer, Content } = Layout;
 
-const MPLayout = () => {
+const contentStyle = {
+  display: 'flex',
+  flexWrap: 'wrap',
+  padding: '3rem 3rem',
+  justifyContent: 'center'
+}
+
+const MPLayout = ( {children} ) => {
   return (
     <Layout>
-        Test
+        <Header>
+          <Menu theme='dark' mode='horizontal' defaultSelectedKeys={['1']}>
+            <Menu.Item key='1'>
+              <Link to='/'>Accueil</Link>
+            </Menu.Item>
+            <Menu.Item key='2'>
+              <Link to='/rooms'>Chambres</Link>
+            </Menu.Item>
+            <Menu.Item key='3'>
+              <Link to='/about'>A Propos</Link>
+            </Menu.Item>
+            <Menu.Item key='4'>
+              <Link to='/contact'>Contact</Link>
+            </Menu.Item>
+          </Menu>
+        </Header>
+
+        <Content style={contentStyle} children={children} />
+
+        <Footer style={{ textAlign: 'center' }}>
+          API Mern React Udemy @2022 MPFJ
+        </Footer>
     </Layout>
   )
 }
